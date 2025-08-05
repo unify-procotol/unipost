3426046c5de2a9dd4f7b0027256d4204240d673e
Jevin_F <jevinfsmail@gmail.com>
2025-08-05 15:28:17 +0800

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   /* config options here */
+  basePath: process.env.NODE_ENV === 'production' ? '/blog1' : '',
   transpilePackages: [
     "@unilab/urpc",
     "@unilab/urpc-core",
